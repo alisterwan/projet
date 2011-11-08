4971b8f77186f99a084bf1121b25e52464111182
Nicolas Stepien <mayhemydg@gmail.com>
2011-11-08 19:01:42 +0100

    Use the ID instead of the username to identify the user as it's unique and cannot be changed; shorter queries, use an associative array, shorter variable names.

--- header.php
+++ header.php
@@ -10,43 +10,44 @@
 
 
   else if ($_POST) {
-    $user = $_POST[username];
-    $pass = sha1($_POST[password]);
+    $name = $_POST[name];
+    $pass = sha1($_POST[pass]);
 
 
   // Rediriger l'admin s'il est correctement identifié
-    if ($user == 'admin' && $pass == 'f6793a9e6ca5356123fe0ab34bb46443894a5edf') {
-      $_SESSION[name] = Admininistrator;
+    if ($name == 'admin' && $pass == 'f6793a9e6ca5356123fe0ab34bb46443894a5edf') {
       $_SESSION[masterpass] = $pass;
       header('Location: admin/index.php');
     }
 
 
     // Vérification du client dans la base de donnée
-    else if (pg_num_rows(pg_query($conn,"SELECT username FROM users WHERE username='$user' and password='$pass'"))) {
-      $message = "<p class='loggedin'>You are successfully logged in. Welcome <a href='./#'>$user</a>.</p>";
-      $_SESSION[name] = $user;
+    else if (pg_num_rows($query = pg_query($conn,"SELECT id_customer FROM users WHERE username='$user' and password='$pass'"))) {
+      $_SESSION[id] = pg_fetch_result($query, 0, 0);
+      $message = "<p class='loggedin'>You are successfully logged in. Welcome <a href='./#'>$name</a>.</p>";
     }
     else {
       $message = "<p class='error'>Username or password incorrect, try again.</p>";
     }
-}
-
-  if ($_SESSION[name] != Admininistrator) {
-    /*
-     * On récupère les infos de l'utilisateur, s'il n'est pas l'administrateur:
-     * $customer[0] -> firstname
-     * $customer[1] -> surname
-     * $customer[2] -> address
-     * $customer[3] -> city
-     * $customer[4] -> country
-     * $customer[5] -> mail
-     * $customer[6] -> username == $_SESSION[name]
-     * $customer[7] -> id_customer
-     */
-    $customer = pg_fetch_row(pg_query($conn,"SELECT firstname,surname,address,city,country,mail,username,id_customer from users where username='$_SESSION[name]'"));
   }
 
+
+
+  if ($_SESSION[id]) {
+    /*
+     * On récupère les infos de l'utilisateur sous forme de tableau associatif:
+     * $user[firstname]
+     * $user[surname]
+     * $user[address]
+     * $user[city]
+     * $user[country]
+     * $user[username]
+     * $user[pass]
+     * $user[mail]
+     * $user[id_customer] == $_SESSION[id]
+     */
+    $user = pg_fetch_assoc(pg_query($conn,"SELECT * from users where id_customer='$_SESSION[id]'"));
+  }
 
 
   function printDocument($title = "DigEat") {
@@ -86,15 +87,15 @@
 
 
   function rightboxContent() {
-    if ($_SESSION[name]) {
+    if ($_SESSION[id]) {
       // Requête qui récupère toutes les coordonnées du client
-      global $customer;
+      global $user;
       $content = "<p>Your account information:</p>
-        <div>$customer[0] $customer[1]</div>
-        <div>$customer[2]</div>
-        <div>$customer[3]</div>
-        <div>$customer[4]</div>
-        <div>$customer[5]</div>
+        <div>$user[firstname] $user[surname]</div>
+        <div>$user[adress]</div>
+        <div>$user[city]</div>
+        <div>$user[country]</div>
+        <div>$user[mail]</div>
         <a href='./profile.php'>My profile</a><br>
         <a href='./modifyaccount.php'>Modify my account</a><br>
         <a href='./image.php'>My albums</a><br>
@@ -107,9 +108,9 @@
       $content = "
         <form action='./index.php' method='post'>
           <div>Log in:</div>
-          <div><input type='text' name='username' placeholder='Username' required></div>
-          <div><input type='password' name='password' placeholder='password' required></div>
-          <div><input type='submit' name='proceed' value='Submit'> <a href='./#'>Forgot your password?</a></div>
+          <div><input type='text' name='name' placeholder='Username' required></div>
+          <div><input type='password' name='pass' placeholder='password' required></div>
+          <div><input type='submit' value='Submit'> <a href='./#'>Forgot your password?</a></div>
         </form>
         <form action='./registration.php' method='post'>
           <div>Register:</div>
