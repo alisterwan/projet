a416658c5006cfd41e387d14ee86ad16cbd07269
John Wan <alisterwan@gmail.com>
2011-12-30 03:12:14 +0100

    displaying recipe list

--- recipes.php
+++ recipes.php
@@ -1,41 +1,6 @@
 <?php
 	include './header.php';
 
-function printRecipes($userid) {
-$recipe = good_query_assoc("SELECT * FROM recipes WHERE id_user = $userid");
-
-	if (!$recipe){
-		
-		return $html="
-		<h4>You haven't got recipes</h4>
-		<div>
-		<ul>
-		<li><small><a href='./newrecipe.php'>Add a new recipe</a></small></li>
-		<li><small><a href='./newingredient.php'>Add a new ingredient</a></small></li>
-		<ul>
-		</div>
-		";
-	}
-	
-	else 
-	
-	{
-		
-		
-		return $html="
-		<a href='./recipe.php?id=$recipe[id]'>$recipe[name_en]</a> 
-		<div>
-		<ul>
-		<li><small><a href='./newrecipe.php'>Add a new recipe</a></small></li>
-		<li><small><a href='./newingredient.php'>Add a new ingredient</a></small></li>
-		<ul>
-		</div>
-		";
-
-	  }
- }
-
-
 
 if (isset($userid)){  // vérification si logué ou pas
 
@@ -44,12 +9,31 @@ if (isset($userid)){  // vérification si logué ou pas
   $useraddinfos=retrieve_user_add_infos($userid);
   
   $html = "<h1>$userinfos[firstname] $userinfos[surname] ($userinfos[username])</h1>
-  <h3>My Profile</h3>
+  <h3>My Recipes</h3>
   <h4>Born in $useraddinfos[date_birth], Works at $useraddinfos[job] Listen to $useraddinfos[music]</h4>
-  
-  ".printRecipes($userid)."
-  
   ";
+
+$query = sprintf("SELECT * FROM recipes WHERE id_user='%s'",
+	mysql_real_escape_string($userid));	
+	$result = mysql_query($query);	
+	
+	while($row=mysql_fetch_row($result)) {
+	$html.="<div><a href='./recipe.php?id=$row[13]'><img src='img/recipes/$row[12]_$row[13].jpg' /><br>$row[0]</a></div>";	
+	}	
+
+
+if (!$query){
+	$html.="<h4>You haven't got recipes</h4>
+	<div>
+	<ul>
+	<li><small><a href='./newrecipe.php'>Add a new recipe</a></small></li>
+	<li><small><a href='./newingredient.php'>Add a new ingredient</a></small></li>
+	<ul>
+	</div>
+	";
+	}
+
+
 
   printDocument('My Recipes');
   
