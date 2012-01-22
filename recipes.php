42f09a46ff1a3d2a2ac95dcceb9c60e5a1ff128f
John Wan <alisterwan@gmail.com>
2012-01-22 11:59:04 +0100

    dunno

--- recipes.php
+++ recipes.php
@@ -8,45 +8,49 @@ if (isset($userid)){  // vérification si logué ou pas
   $userinfos=retrieve_user_infos($userid);
   $useraddinfos=retrieve_user_add_infos($userid);
   
-  $html = "<h1>$userinfos[firstname] $userinfos[surname] ($userinfos[username])</h1>
-  <h3>My Recipes</h3>
-  <h4>Born in $useraddinfos[date_birth], Works at $useraddinfos[job] Listen to $useraddinfos[music]</h4>
-  <div>
+  $html = "
+<script type='text/javascript'>
+			$(document).ready(function () {
+			$('ul.menu_body li:even').addClass('alt');
+    		$('img.menu_head').click(function () {
+			$('ul.menu_body').slideToggle('medium');
+    		});
+			$('ul.menu_body li a').mouseover(function () {
+			$(this).animate({ fontSize: '14px', paddingLeft: '20px' }, 50 );
+    		});
+			$('ul.menu_body li a').mouseout(function () {
+			$(this).animate({ fontSize: '12px', paddingLeft: '10px' }, 50 );
+    			});
+			});
+			</script>
+
+	<h1>$userinfos[firstname] $userinfos[surname] ($userinfos[username])</h1>
+  	<h3>My Recipes</h3>
+	<img src='img/templates/option.png' width='150' height='40' class='menu_head' />
+		<ul class='menu_body'>
+    		<li><a href='./newrecipe.php'>Add Recipe</a></li>
+    		<li><a href='./newingredient.php'>Add Ingredients</a></li>
+
+		</ul>
+		
+		
+
   ";
 
 $query = sprintf("SELECT * FROM recipes WHERE id_user='%s'",
 	mysql_real_escape_string($userid));	
 	$result = mysql_query($query);	
 	
-	while($row=mysql_fetch_row($result)) {
-	$html.="<div><a href='./recipe.php?id=$row[13]'><img src='img/recipes/$row[12]_$row[13].jpg' width='200px' height='180px'/><br>$row[0]</a></div>";	
-	}	
-
-
-if (!$query){
+	if (!$result){
 	$html.="
-	</div>
-	<h4>You haven't got recipes</h4>
-	<div>
-	<ul>
-	<li><small><a href='./newrecipe.php'>Add a new recipe</a></small></li>
-	<li><small><a href='./newingredient.php'>Add a new ingredient</a></small></li>
-	<ul>
-	</div>
-	";
+	<p>You haven't got recipes</p>";
 	}
 	
 
-	 $html.="
-	</div>
-	<div>
-	<ul>
-	<li><small><a href='./newrecipe.php'>Add a new recipe</a></small></li>
-	<li><small><a href='./newingredient.php'>Add a new ingredient</a></small></li>
-	<ul>
-	</div>
-	";		
 	
+	while($row=mysql_fetch_row($result)) {
+	$html.="<div><a href='./recipe.php?id=$row[13]'><img src='img/recipes/$row[12]_$row[13].jpg' width='250px' height='220px' alt='$row[0]' title='$row[0]'/><br/>$row[0]</a></div>";	
+	}	
 
 
 
