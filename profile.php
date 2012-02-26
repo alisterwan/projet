6e679882f223dde1a82f701bf93fac2b6677713d
Video Bulle <bien.manger@live.fr>
2012-02-27 00:33:40 +0100

    Display modified

--- profile.php
+++ profile.php
@@ -14,6 +14,41 @@ La fonction checkPermission($vargroup, $user) renvoie TRUE si le $user appartien
 printAddNewFriend($userid) imprime les boutons pour ajouter en ami ou follow. $userid est l'ID du demandeur.
 /**************************************************/
 
+function leftboxContent() {
+	if (isset($_SESSION['id'])) {
+		if (isset($_GET['id_user']) ) { 
+			// Requête qui récupère toutes les coordonnées du client
+			$userinfos=retrieve_user_infos($_GET['id_user']);
+			$content = "<img src= '$userinfos[avatar]' width='170px' height='200px' />";
+
+    		return $content;
+		}else{
+		// Requête qui récupère toutes les coordonnées du client
+			global $userid;
+			$userinfos=retrieve_user_infos($userid);
+			$content = "<img src= '$userinfos[avatar]' width='170px' height='200px' /><a href='./image.php'><img src= './img/templates/camera.png' width='50px' height='50px'/></a>Change my avatar";
+			$content.="
+			<div class='stack'>
+			<img src='img/stacks/stack.png' alt='stack'/>
+			<ul id='stack'>
+				<li><a href='objectivesform.php'><span>Objectives</span><img src='img/stacks/objectives.png' alt='My Objectives' /></a></li>
+				<li><a href='information.php'><span>Information</span><img src='img/stacks/information.png' alt='My infos' /></a></li>			
+				<li><a href='albums.php'><span>Albums</span><img src='img/stacks/albums.png' alt='My albums' /></a></li>
+				<li><a href='friends.php'><span>Friends</span><img src='img/stacks/myfriends.png' alt='My friends' /></a></li>	
+				<li><a href='recipes.php'><span>Recipes</span><img src='img/stacks/recipes.png' alt='My recipes' /></a></li>				
+			</ul>
+			</div>";
+			return $content;
+		}
+    }else if (isset($_GET['id_user'])) { 
+		// Requête qui récupère toutes les coordonnées du client
+		$userinfos=retrieve_user_infos($_GET['id_user']);
+		$content = "<img src= '$userinfos[avatar]' width='170px' height='200px' />";
+		return $content;
+    }
+}
+
+
 function printInfoMember($id){
 	$infos = retrieve_user_add_infos($id);
 	$ficelle = "<h4>";
