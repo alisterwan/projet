b816fb8f0152ed4e5df938a8bd353142fb4db6dd
John Wan <alisterwan@gmail.com>
2012-02-27 01:13:26 +0100

    fixing some bugs on adding friends

--- addFriends.php
+++ addFriends.php
@@ -2,19 +2,15 @@
   include './header.php';
   header('Content-type: text/plain');
 
-	
-  $userfriends = retrieve_user_friends($userid);
-  $groupnum = getGroupId($userid);
-  
-  	$query2 ="INSERT INTO groups_relations(id_group,id_user,approval) VALUES ($groupnum,$_GET[id_friend],0)";
+
 		
-		$res2 = @mysql_query($query2);
+		
+		$res2 = @mysql_query("INSERT INTO groups_relations(id_group,id_user,approval) VALUES ($_GET[id_user],$_GET[id_friend],0)");
 		
 		if(!$res2)
 			die("Error: ".mysql_error());
 		else
-			 echo "<img src='./img/templates/pendingfriend.png' width='113px' height='42px' />";	
+			 echo "
+			 <img src='./img/templates/pendingfriend.png' width='113px' height='42px' />";	
  
-
-  
 ?>
