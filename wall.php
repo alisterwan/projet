a0a239b159666f753d623edcee3fc948baea7708
Video Bulle <bien.manger@live.fr>
2012-03-26 11:39:18 +0200

    ready for CSS

--- wall.php
+++ wall.php
@@ -145,15 +145,12 @@ function getCommentsIdByWallPostId($postid){ // return array of comment id from 
 }
 
 function getGroupsByWallPostId($id){ // return allowed groups by Post ID /// False if NONE
-	//$query = sprintf("SELECT * FROM wall_post_permission WHERE id_wall_post='%s'",	mysql_real_escape_string($id));
 	$query = 'SELECT id_group FROM wall_post_permission WHERE id_wall_post='.$id;
 	$result = mysql_query($query);
-	global $message;
 	if (!$result) {
 		return false;
 	}else{
 		if(mysql_num_rows($result)<1) return false; // no group
-
 		$reponse;
 		while ($row = mysql_fetch_assoc($result)) {
 			$reponse[]=$row['id_group'];
@@ -349,9 +346,12 @@ function printAllPostByUserId($id){ // display ALL POSTS AND COMMENTS according 
 	if(!$allposts) return false; // no post
 
 	$ficelle = '';
-	foreach($allposts AS $post){
-		$ficelle.= printWallPostById($post).'<br><br>';
+	$ficelle.= '<div class="all_wall_post" >';
+	
+		foreach($allposts AS $post){
+		$ficelle.= printWallPostById($post).'<br>';
 	}
+	$ficelle.= '</div>';
 	return $ficelle;
 }
 
@@ -370,16 +370,22 @@ function printWallPostById($idpost){ // display a Post and Comments
 	$date = $result['date'];
 
 	$ficelle ='';
-	$ficelle.= '<table style="text-align: left; " border="1" cellpadding="2" cellspacing="2">';
-	$ficelle.= '<tr><td>'.printAvatarByUserId($id_user).'<br/>';// case avatar
-	$ficelle.= printRatingPost($idpost).'</td>'; // ratings
+	$ficelle.= '<div class="wall_post" >';
+	
+	if(isset($_GET['id']) && userIdExists($_GET['id'])){ // creating form
+		$ficelle.= '<form action="wall.php?id='.$_GET['id'].'" method="POST" >';
+	}else{
+		$ficelle.= '<form action="wall.php" method="POST" >';
+	}
+	
+	$ficelle.= printAvatarByUserId($id_user).'<br/>';// case avatar
+	$ficelle.= printRatingPost($idpost); // ratings
 
-	$ficelle.= '<td>'.printLinkToProfileByUserId($id_user).' - '.$date.'<br/>';
+	$ficelle.= printLinkToProfileByUserId($id_user).' - '.$date.'<br/>';
 	$ficelle.= $content.'<hr/>';
 	$ficelle.= printLikeDislikePost($id_post).'<br/>'.printPermissionChoices($id_post, getCreatorIdByPostId($id_post)).'<hr/>';
 	$ficelle.= printAllCommentsByWallPostId($id_post);
-	$ficelle.= '</td></tr></table>';
-
+	$ficelle.= '</form></div>';
 	return $ficelle;
 }
 
@@ -396,20 +402,13 @@ function printLikeDislikePost($idpost){ // like dislike delete bar for POST
 	$wallowner = getCreatorIdByPostId($idpost);
 	if(!alreadyRatedPostByUser($idpost, $userid)){
 		// Like button
-		if($wallowner!=$userid){
-			$ficelle.= '<form action="wall.php?id='.$wallowner.'" method="POST" >';
-		}else{
-			$ficelle.= '<form action="wall.php" method="POST" >';
-		}
-		$ficelle.= '<input type="submit" value="Like" /><input type="hidden" name="rating_post_like" id="rating_post_like" value='.$idpost.' /></form>';
-
+		$ficelle.= '<button class="like_button" title="Like post" value='.$idpost.' name="post_like" type="submit">
+					Like
+					</button>';
 		// Dislike button
-		if($wallowner!=$userid){
-			$ficelle.= '<form action="wall.php?id='.$wallowner.'" method="POST" >';
-		}else{
-			$ficelle.= '<form action="wall.php" method="POST" >';
-		}
-		$ficelle.= '<input type="submit" value="Dislike" /><input type="hidden" name="rating_post_dislike" id="rating_post_dislike" value='.$idpost.' /></form>';
+		$ficelle.= '<button class="dislike_button" title="Dislike post" value='.$idpost.' name="post_dislike" type="submit">
+					Dislike
+					</button>';
 	}else{ // undo rating?
 		if(getCurrentUserPostRatingByPost($idpost)==0){
 			$ficelle.='You dislike this post. ';
@@ -417,29 +416,17 @@ function printLikeDislikePost($idpost){ // like dislike delete bar for POST
 			$ficelle.='You like this post. ';
 		}
 
-		if($wallowner!=$userid){
-			$ficelle.= '<form action="wall.php?id='.$wallowner.'" method="POST" >';
-		}else{
-			$ficelle.= '<form action="wall.php" method="POST" >';
-		}
-		$ficelle.='<input type="submit" value="Undo" /><input type="hidden" name="undo_post_rating" id="undo_post_rating" value='.$idpost.' /></form>';
+		$ficelle.= '<button class="undo_button" title="Undo post rating" value='.$idpost.' name="post_undo_rating" type="submit">
+					Undo
+					</button>';
 	}
 
 	// delete button
 	if(posterOrOwnerOfPost($idpost, $userid)){
-		if($wallowner!=$userid){
-			$ficelle.= '<form action="wall.php?id='.$wallowner.'" method="POST" >';
-		}else{
-			$ficelle.= '<form action="wall.php" method="POST" >';
-		}
-		$ficelle.= '<input type="submit" value="Delete" /><input type="hidden" name="delete_post" id="delete_post" value='.$idpost.' /></input></form>';
+		$ficelle.= '<button class="delete_button" title="Delete post" value='.$idpost.' name="post_delete" type="submit">
+					Delete
+					</button>';
 	}
-
-	// date
-	$sql = 'SELECT date FROM wall_post WHERE id='.$idpost;
-	$query = mysql_query($sql);
-	$result = mysql_fetch_assoc($query);
-	$ficelle.= $result['date'];
 
 	return $ficelle;
 }
@@ -454,13 +441,12 @@ function printCommentByCommentId($idcomment){ // comment display
 
 	$result = mysql_fetch_assoc($query);
 
-	$ficelle = '<table style="text-align: left; " border="1" cellpadding="2" cellspacing="2">';
-	$ficelle.= '<tr><td>'.printAvatarByUserId($result['id_poster']).'<br/>'; // case avatar
-	$ficelle.= printRatingComment($idcomment).'</td>'; // ratings
+	$ficelle = '<div class="wall_post_comment" >'.printAvatarByUserId($result['id_poster']).'<br/>'; // case avatar
+	$ficelle.= printRatingComment($idcomment); // ratings
 
-	$ficelle.= '<td>'.printLinkToProfileByUserId($result['id_poster']).' '.$result['comment'].'<br/><br/>';
-	$ficelle.= printLikeDislikeComment($idcomment).'</td>';
-	$ficelle.= '</tr></table><br/>';
+	$ficelle.= printLinkToProfileByUserId($result['id_poster']).' '.$result['comment'].'<br/>';
+	$ficelle.= printLikeDislikeComment($idcomment);
+	$ficelle.= '</div>';
 
 	return $ficelle;
 }
@@ -470,7 +456,9 @@ function printAllCommentsByWallPostId($postid){ // all comments for a post
 	$ficelle ='';
 	$sql = 'SELECT * FROM wall_post_comment WHERE id_wall_post='.$postid;
 	$query = mysql_query($sql);
-
+	
+	$ficelle.= '<div class="all_wall_post_comment" >';
+	
 	$nbcomment = mysql_num_rows($query);
 	if($nbcomment>1){
 		$ficelle.= $nbcomment.' comments';
@@ -478,7 +466,14 @@ function printAllCommentsByWallPostId($postid){ // all comments for a post
 		$ficelle.= $nbcomment.' comment';
 	}
 	$ficelle.= '<br/>';
-
+	
+	$wallowner = getCreatorIdByPostId($postid);
+	if($wallowner!=$userid){
+		$ficelle.= '<form action="wall.php?id='.$wallowner.'" method="POST" >';
+	}else{
+			$ficelle.= '<form action="wall.php" method="POST" >';
+	}
+	
 	////////////////PRINT ALL COMMENTS/////////////
 	$commentsID = getCommentsIdByWallPostId($postid);
 	if($commentsID!=false){
@@ -487,16 +482,11 @@ function printAllCommentsByWallPostId($postid){ // all comments for a post
 		}
 	}
 	///////////////NEW COMMENT/////////////
-	$wallowner = getCreatorIdByPostId($postid);
-	if($wallowner!=$userid){
-		$ficelle.= '<form action="wall.php?id='.$wallowner.'" method="POST" >';
-	}else{
-			$ficelle.= '<form action="wall.php" method="POST" >';
-	}
 	$ficelle.= '<input type="hidden" name="idpost" id="idpost" value='.$postid.' />
-	<textarea name="newcomment" id="newcomment" /></textarea><br/>
+	<textarea name="newcomment" id="newcomment" ></textarea><br/>
 	<input type="submit" value="Post new comment" /></form>';
-
+	$ficelle.= '</div>';
+	
 	return $ficelle;
 }
 
@@ -507,76 +497,70 @@ function printLikeDislikeComment($idcomment){ // like dislike delete bar for com
 	$wallowner = getWallOwnerByCommentId($idcomment);
 	if(!alreadyRatedCommentByUser($idcomment, $userid)){
 		// Like button
-		if($wallowner!=$userid){
-			$ficelle.= '<form action="wall.php?id='.$wallowner.'" method="POST" >';
-		}else{
-			$ficelle.= '<form action="wall.php" method="POST" >';
-		}
-		$ficelle.= '<input type="submit" value="Like" /><input type="hidden" name="rating_comment" id="rating_comment" value='.$idcomment.' />
-		<input type="hidden" name="rating" id="rating" value="1" /></form>';
+		$ficelle.= '<button class="like_button" title="Like this" value='.$idcomment.' name="comment_like" type="submit">
+					Like
+					</button>';
 
-		// Dislike button
-		if($wallowner!=$userid){
-			$ficelle.= '<form action="wall.php?id='.$wallowner.'" method="POST" >';
-		}else{
-			$ficelle.= '<form action="wall.php" method="POST" >';
-		}
-		$ficelle.= '<input type="submit" value="Dislike" /><input type="hidden" name="rating_comment" id="rating_comment" value='.$idcomment.' />
-		<input type="hidden" name="rating" id="rating" value="0" /></form>';
+		// Dislike button		
+		$ficelle.= '<button class="dislike_button" title="Dislike this" value='.$idcomment.' name="comment_dislike" type="submit">
+					Dislike
+					</button>';
 	}else{ // undo rating?
 		if(getCurrentUserPostRatingByComment($idcomment)==0){
 			$ficelle.='You dislike this comment. ';
 		}else{
 			$ficelle.='You like this comment. ';
 		}
-		if($wallowner!=$userid){
-			$ficelle.= '<form action="wall.php?id='.$wallowner.'" method="POST" >';
-		}else{
-			$ficelle.= '<form action="wall.php" method="POST" >';
-		}
-		$ficelle.='<input type="submit" value="Undo" /><input type="hidden" name="undo_comment_rating" id="undo_comment_rating" value='.$idcomment.' /></form>';
+		$ficelle.= '<button class="undo_button" title="Undo rating" value='.$idcomment.' name="comment_undo_rating" type="submit">
+					Undo
+					</button>';
 	}
 
 	// delete button
 	if(posterOrOwnerOfComment($idcomment, $userid)){
-		if($wallowner!=$userid){
-			$ficelle.= '<form action="wall.php?id='.$wallowner.'" method="POST" >';
-		}else{
-			$ficelle.= '<form action="wall.php" method="POST" >';
-		}
-		$ficelle.= '<input type="submit" value="Delete" /><input type="hidden" name="delete_comment" id="delete_comment" value='.$idcomment.' /></input></form>';
+		$ficelle.= '<button class="delete_button" title="Delete comment" value='.$idcomment.' name="comment_delete" type="submit">
+					Delete comment
+					</button>';
 	}
-
+	
 	// date
 	$sql = 'SELECT date FROM wall_post_comment WHERE id='.$idcomment;
 	$query = mysql_query($sql);
 	$result = mysql_fetch_assoc($query);
-	$ficelle.= $result['date'];
+	$ficelle.= '<br/>'.$result['date'];
 
 	return $ficelle;
 }
 
 function printPermissionChoices($idpost, $iduser){ // display Permissions and choices
-	global $userid;
+	global $userid; 
 	if($userid != $iduser) return '';
-
-	$ficelle ='<form action="wall.php" method="post" >';
+	$ficelle = '';
 	$ficelle.= '<label for="share_with_group" >Share with</label> <select name="share_with_group" id="share_with_group">';
 
 	$groupsid = getAllGroupsByUserId($iduser); // all groups of iduser
 	$allowedGroupsId = getGroupsByWallPostId($idpost); // all groups granted by iduser
-
-	foreach($groupsid AS $group){ // add only non granted groups
-		if(!in_array($group, $allowedGroupsId)) $ficelle.= '<option value="'.$group.'">'.getGroupNameById($group).'</option>';
+	if(is_array($allowedGroupsId)){ // at least 1 group is granted
+		foreach($groupsid AS $group){ // add only non granted groups
+			if(!in_array($group, $allowedGroupsId)) $ficelle.= '<option value="'.$group.'">'.getGroupNameById($group).'</option>';
+		}
+	}else{ // no group granted
+		foreach($groupsid AS $group){ // add only non granted groups
+			$ficelle.= '<option value="'.$group.'">'.getGroupNameById($group).'</option>';
+		}
 	}
 
-	$ficelle.= '</select> <input type="submit" value="Submit" /><input type="hidden" name="post_allowed" id="post_allowed" value='.$idpost.' /></form>';
+	$ficelle.= '</select> <input type="submit" value="Submit" /><input type="hidden" name="post_allowed" id="post_allowed" value='.$idpost.' /><br/>';
 
 	if($allowedGroupsId!=false ){
 		foreach($allowedGroupsId AS $allowedGroup){
-			$ficelle.= '<form action="wall.php" method="post" >'.getGroupNameById($allowedGroup).'<input type="hidden" name="remove_permission" id ="remove_permission" value='.$allowedGroup.' />
-			<input type="hidden" name="post_restricted" id ="post_restricted" value='.$idpost.' />
-			<input type="submit" value="Remove" /></form>';
+			$ficelle.= getGroupNameById($allowedGroup).'<input type="hidden" name="remove_permission" id ="remove_permission" value='.$allowedGroup.' />
+			';
+			//<input type="hidden" name="post_restricted" id ="post_restricted" value='.$idpost.' />';
+			//<input type="submit" value="Remove" /><br/>';
+			$ficelle.= '<button class="remove_group_button" title="Remove access" value='.$idpost.' name="post_restricted" type="submit">
+					Remove
+					</button>';
 		}
 	}
 	return $ficelle;
@@ -641,33 +625,33 @@ if(isset($userid) || isVisitor() ){
 			$query = mysql_query($sql); // insert new permission (Friends default)
 		}
 
-		if(isset($_POST['rating_post_like'])){ // Post rating LIKE
-			$idpost = $_POST['rating_post_like'];
+		if(isset($_POST['post_like']) && !alreadyRatedPostByUser($_POST['post_like'], $userid)){ // Post rating LIKE
+			$idpost = $_POST['post_like'];
 			$sql = 'INSERT INTO wall_post_rating(id_post, id_user, rating)
 			VALUES('.$idpost.', '.$userid.', 1)';
 			$query = mysql_query($sql); // Post liked
 		}
 
-		if(isset($_POST['rating_post_dislike'])){ // Post rating DISLIKE
-			$idpost = $_POST['rating_post_dislike'];
+		if(isset($_POST['post_dislike']) && !alreadyRatedPostByUser($_POST['post_dislike'], $userid)){ // Post rating DISLIKE
+			$idpost = $_POST['post_dislike'];
 			$sql = 'INSERT INTO wall_post_rating(id_post, id_user, rating)
 			VALUES('.$idpost.', '.$userid.', 0)';
 			$query = mysql_query($sql); // Post disliked
 		}
 
-		if(isset($_POST['undo_post_rating'])){ // post rating UNDO
-			$idpost = $_POST['undo_post_rating'];
+		if(isset($_POST['post_undo_rating'])){ // post rating UNDO
+			$idpost = $_POST['post_undo_rating'];
 			$sql = 'DELETE FROM wall_post_rating WHERE id_user='.$userid.' AND id_post='.$idpost;
 			$query = mysql_query($sql); // Post rating UNDONE
 		}
 
-		if(isset($_POST['delete_post'])){ // delete post
-			$idpost = $_POST['delete_post'];
+		if(isset($_POST['post_delete'])){ // delete post
+			$idpost = $_POST['post_delete'];
 			$sql = 'DELETE FROM wall_post WHERE id='.$idpost;
 			$query = mysql_query($sql); // Post DELETED
 		}
 
-		if(isset($_POST['share_with_group']) && isset($_POST['post_allowed'])){ // grant access to group
+		if(isset($_POST['share_with_group']) && isset($_POST['post_allowed']) && !isset($_POST['post_restricted'])){ // grant access to group
 			$idgroup = $_POST['share_with_group'];
 			$idpost = $_POST['post_allowed'];
 			$sql = 'INSERT INTO wall_post_permission(id_wall_post, id_group) VALUES('.$idpost.', '.$idgroup.')';
@@ -683,7 +667,7 @@ if(isset($userid) || isVisitor() ){
 
 		////////////////////////////// COMMENT MODIFICATIONS!!!! //////////////////////////////////////
 
-		if(isset($_POST['newcomment']) && isset($_POST['idpost'])){ // NEW COMMENT!!
+		if(isset($_POST['newcomment']) && isset($_POST['idpost']) && $_POST['newcomment']!=''){ // NEW COMMENT!!
 			$comment = $_POST['newcomment'];
 			$idpost = $_POST['idpost'];
 			if(isset($_GET['id']) && $_GET['id']!=$userid){ // poster is not owner
@@ -696,22 +680,27 @@ if(isset($userid) || isVisitor() ){
 			$query = mysql_query($sql); // comment posted
 		}
 
-		if(isset($_POST['rating_comment']) && isset($_POST['rating'])){ // Rate a comment
-			$idcomment = $_POST['rating_comment'];
-			$rating = $_POST['rating'];
+		if(isset($_POST['comment_like']) && !alreadyRatedCommentByUser($_POST['comment_like'], $userid)){ // Rate a comment
+			$idcomment = $_POST['comment_like'];
+			$message = 'comment like n°'.$idcomment;
 			$sql = 'INSERT INTO wall_post_comment_rating(id_comment, id_user, rating)
-			VALUES('.$idcomment.', '.$userid.', '.$rating.')';
+			VALUES('.$idcomment.', '.$userid.', 1)';
+			$query = mysql_query($sql); // comment rated
+		}elseif(isset($_POST['comment_dislike']) && !alreadyRatedCommentByUser($_POST['comment_dislike'], $userid)){
+			$idcomment = $_POST['comment_dislike'];
+			$sql = 'INSERT INTO wall_post_comment_rating(id_comment, id_user, rating)
+			VALUES('.$idcomment.', '.$userid.', 0)';
 			$query = mysql_query($sql); // comment rated
 		}
 
-		if(isset($_POST['undo_comment_rating'])){ // undo comment rating
-			$idcomment = $_POST['undo_comment_rating'];
+		if(isset($_POST['comment_undo_rating'])){ // undo comment rating
+			$idcomment = $_POST['comment_undo_rating'];
 			$sql = 'DELETE FROM wall_post_comment_rating WHERE id_user='.$userid.' AND id_comment='.$idcomment;
 			$query = mysql_query($sql); // Comment rating UNDONE
 		}
 
-		if(isset($_POST['delete_comment'])){ // delete post
-			$idcomment = $_POST['delete_comment'];
+		if(isset($_POST['comment_delete'])){ // delete post
+			$idcomment = $_POST['comment_delete'];
 			$sql = 'DELETE FROM wall_post_comment WHERE id='.$idcomment;
 			$query = mysql_query($sql); // Comment DELETED
 		}
