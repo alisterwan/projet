50429dc6ed4841cbe3840529edc11510eece5f4c
Nicolas Stepien <stepien.nicolas@gmail.com>
2012-03-29 17:28:06 +0200

    Fixes in friends.php and auto-completion positions.

--- friends.php
+++ friends.php
@@ -34,7 +34,7 @@ function isFriend(){
 		if(isLoggedVisitor()){
 			return checkPermission(getAllGroupsExceptFollowersByUserId($_GET['id']), $userid);
 		}
-	}	
+	}
 	return false;
 }
 
@@ -49,7 +49,7 @@ function isVisitor(){
 function isLost(){ // non connected and not visiting anything lol
 	global $userid;
 	if(!isConnected() && !isVisitor()) return true;
-	
+
 	return false;
 }
 
@@ -62,10 +62,10 @@ function isOwner(){
 function leftboxContent(){
 	$content ='';
 	global $userid;
-	
+
 	if(!isLost()){
 		if (isset($_SESSION['id'])) { // if logged in
-			if (isLoggedVisitor()) { // if visitor 
+			if (isLoggedVisitor()) { // if visitor
 				// Requête qui récupère toutes les coordonnées du client
 				$userinfos=retrieve_user_infos($_GET['id']);
 				$content.= "<img src= '$userinfos[avatar]' width='170px' height='200px' />";
@@ -74,22 +74,22 @@ function leftboxContent(){
 				global $userid;
 				$userinfos=retrieve_user_infos($userid);
 				$content.= "<img src= '$userinfos[avatar]' width='170px' height='200px'><a href='./image.php'><img src= './img/templates/camera.png' width='50px' height='50px'></a>Change my avatar";
-				
+
 			}
 		}else if(isset($_GET['id'])){ // non logged in visitor
 			// Requête qui récupère toutes les coordonnées du client
 			$userinfos=retrieve_user_infos($_GET['id']);
 			$content.= "<img src= '$userinfos[avatar]' width='170px' height='200px' />";
 		}
-		
+
 		$content.= '<br/><br/>';
-		
+
 		if(isVisitor()){ // print link to Profile
 			$content.= '<a href="profile.php?id_user='.$_GET['id'].'" >Profile</a>';
 		}else{
 			$content.= '<a href="profile.php" >Profile</a>';
-		}		
-		$content.= '<br/>';		
+		}
+		$content.= '<br/>';
 		if(isVisitor()){ // print link to wall
 			$content.= '<a href="wall.php?id='.$_GET['id'].'" >Wall</a>';
 		}else{
@@ -101,7 +101,7 @@ function leftboxContent(){
 		}else{
 			$content.= '<a href="friends.php" >Friends list</a>';
 		}
-		$content.= '<br/>';	
+		$content.= '<br/>';
 		if(isFriend()){ // print link to messages
 			$content.= '<a href="private_messages.php?id_recipient='.$_GET['id'].'" >Private Messages</a>';
 			$content.= '<br/>';
@@ -113,76 +113,10 @@ function leftboxContent(){
 			$content.= '<br/>';
 			$content.= '<a href="fridge.php" >Fridge</a>';
 			$content.= '<br/>';
-			$content.= '<a href="shoplist.php" >Shoplist</a>';	
+			$content.= '<a href="shoplist.php" >Shoplist</a>';
 		}
 		$content.= '<br/>';
 	}
-	return $content;
-}
-
-
-function rightboxContent() {
-	// ATTENTION IL FAUT METTRE LES QUOTES POUR id
-	if (isset($_SESSION['id'])) {
-		// Requête qui récupère les infos de l'utilisteur.
-		$userinfos = retrieve_user_infos($_SESSION['id']);
-
-		$content = "";
-
-		$countryname = getCountryNameById($userinfos['country']);
-
-		$content.= "<p>Your account information:</p>
-		<div>$userinfos[firstname] &nbsp;$userinfos[surname]</div>
-		<div>$userinfos[address]</div>
-		<div>$userinfos[city]</div>
-		<div>$countryname</div>
-		<div>$userinfos[mail]</div>
-		<div class='stack'>
-			<img src='img/stacks/stack.png' alt='stack'>
-			<ul id='stack'>
-				<li>
-					<a href='objectivesform.php'>
-						<span>Objectives</span>
-						<img src='img/stacks/objectives.png' alt='My Objectives'>
-					</a>
-				</li>
-				<li>
-					<a href='information.php'>
-						<span>Information</span>
-						<img src='img/stacks/information.png' alt='My infos'>
-					</a>
-				</li>
-				<li>
-					<a href='albums.php'>
-						<span>Albums</span>
-						<img src='img/stacks/albums.png' alt='My albums'>
-					</a>
-				</li>
-				<li>
-					<a href='friends.php'>
-						<span>Friends</span>
-						<img src='img/stacks/myfriends.png' alt='My friends'>
-					</a>
-				</li>
-				<li>
-					<a href='recipes.php'>
-						<span>Recipes</span>
-						<img src='img/stacks/recipes.png' alt='My recipes'>
-					</a>
-				</li>
-			</ul>
-		</div>";
-	} else {
-		// Afficher les formulaires pour se connecter et s'inscrire.
-		$content = "
-		<form action='./index.php?mode=logon' method='post'>
-			<img src='./img/templates/login.png'>
-			<div><input type='text' name='name' placeholder='Username' required></div>
-			<div><input type='password' name='pass' placeholder='password' required></div>
-			<div><input type='submit' value='Submit'> <a href='./#'>Forgot your password?</a></div>
-		</form>";
-	}
-
 	return $content;
 }
 
@@ -204,7 +138,7 @@ function printUserBadgeById($id) {
 		<tr>
 			<td>'.printAvatarBadgeByURL($user['avatar']).'</td>
 			<td><a href=profile.php?id_user='.$user['id'].'>'.$user['firstname'].' '.$user['surname'].'<br>';
-			
+
 	global $userid;
 
 	if (!belongsToUserGroups($userid, $user['id'])) {
@@ -296,15 +230,10 @@ if (isConnected() && !isset($_GET['id'])) {
 
 	$html = "
 	<h1>$userinfos[firstname] $userinfos[surname] ($userinfos[username])</h1>
-	<div id='content'>
-		<div id='dock'>
-			<div class='dock-container'>
-				<a class='dock-item' href='newmessage.php'><span>Messages</span><img src='img/dock/email.png' alt='messages'></a>
-				<a class='dock-item' href='groups.php'><span>Groups</span><img src='img/dock/portfolio.png' alt='history'></a>
-				<a class='dock-item' href='followers.php'><span>Followers</span><img src='img/dock/link.png' alt='links'></a>
-				<a class='dock-item' href='#'><span>RSS</span><img src='img/dock/rss.png' alt='rss'></a>
-			</div>
-		</div>
+	<div class='navlinks'>
+		<a href='newmessage.php'>Messages</a>
+		<a href='groups.php'>Groups</a>
+		<a href='followers.php'>Followers</a>
 	</div>
 	<h3>My Contacts</h3>";
 
